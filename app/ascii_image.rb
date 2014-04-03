30d807e07362dd7f52d1e6ee1ec59997622a2f63
Jamey J. DeOrio <jameyd@darkhorse.com>
2014-04-02 21:36:04 -0700

    Get these images chunkin'

--- app/ascii_image.rb
+++ app/ascii_image.rb
@@ -8,13 +8,27 @@ class AsciiImage
     @raw = Magick::ImageList.new(image_filename)[0]
   end
 
-  def grayscale
+  def monochrome
     clone = self.clone
     clone.raw = clone.raw.quantize(2, Magick::GRAYColorspace)
     return clone
   end
 
-  def grayscale!
+  def monochrome!
     @raw = @raw.quantize(2, Magick::GRAYColorspace)
+  end
+
+  def chunk(chunk_size=12)
+    chunk_columns = @raw.columns / chunk_size
+    chunk_rows = @raw.rows / chunk_size
+    chunks = []
+    chunk_rows.times do |row_n|
+      row = []
+      chunk_columns.times do |col_n|
+        row << @raw.crop(col_n * chunk_size, row_n * chunk_size, chunk_size, chunk_size)
+      end
+      chunks << row
+    end
+    return chunks
   end
 end
