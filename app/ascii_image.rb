36077733e6f1fd19a0434e6e1570ee14e0760b18
Jamey J. DeOrio <jameyd@darkhorse.com>
2014-04-02 21:07:43 -0700

    Allow images to be converted to b&w

--- app/ascii_image.rb
+++ app/ascii_image.rb
@@ -2,7 +2,19 @@ require 'rmagick'
 require 'optparse'
 
 class AsciiImage
+  attr_accessor :raw
+
   def initialize(image_filename)
-    @raw_image = Magick::ImageList.new(image_filename)[0]
+    @raw = Magick::ImageList.new(image_filename)[0]
+  end
+
+  def grayscale
+    clone = self.clone
+    clone.raw = clone.raw.quantize(2, Magick::GRAYColorspace)
+    return clone
+  end
+
+  def grayscale!
+    @raw = @raw.quantize(2, Magick::GRAYColorspace)
   end
 end
--- tests/test_ascii_image.rb
+++ tests/test_ascii_image.rb
@@ -6,10 +6,15 @@ class TestAsciiImage < MiniTest::Test
     @image = AsciiImage.new('tests/test_pattern.gif')
   end
 
-  def test_image_can_be_created
+  def test_image_can_be_grayscaled
+    refute @image.raw.gray?, 'image must begin in color'
+    @image = @image.grayscale
+    assert @image.raw.gray?, 'image was not converted to black and white'
   end
 
-  def test_image_can_be_grayscaled
-    @image.grayscale
+  def test_image_can_be_grayscaled_dangerous
+    refute @image.raw.gray?, 'image must begin in color'
+    @image.grayscale!
+    assert @image.raw.gray?, 'image was not converted to black and white'
   end
 end
