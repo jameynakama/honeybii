80e4689e3dd9f76562aa762c616ec09937839e78
Jamey J. DeOrio <jameyd@darkhorse.com>
2014-04-02 20:20:10 -0700

    Replace test image

--- tests/test_ascii_image.rb
+++ tests/test_ascii_image.rb
@@ -3,9 +3,13 @@ require_relative '../app/ascii_image'
 
 class TestAsciiImage < MiniTest::Test
   def setup
-    @image = AsciiImage.new('tests/test.png')
+    @image = AsciiImage.new('tests/test_pattern.gif')
   end
 
   def test_image_can_be_created
+  end
+
+  def test_image_can_be_grayscaled
+    @image.grayscale
   end
 end
